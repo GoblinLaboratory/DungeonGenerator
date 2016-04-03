6b8efc12d330cf199ffcb90bbb974670337612a6
Supultura <supulturastreaming@gmail.com>
2016-04-03 21:02:25 +1000

    fixed CreateDungeon, now generates a dungeon of any size

--- DungeonGenerator/CreateDungeon.cs
+++ DungeonGenerator/CreateDungeon.cs
@@ -9,26 +9,56 @@ namespace DungeonGenerator
 {
 	public class CreateDungeon
 	{
+		public static string PrintPiece(Piece p)
+		{
+			string sPiece = "(";
+			if (p.ConnectBottom == true)
+				sPiece += "true, ";
+			else if (p.ConnectBottom == false)
+				sPiece += "false, ";
+			else
+				sPiece += "null, ";
+			if (p.ConnectTop == true)
+				sPiece += "true, ";
+			else if (p.ConnectTop == false)
+				sPiece += "false, ";
+			else
+				sPiece += "null, ";
+			if (p.ConnectLeft == true)
+				sPiece += "true, ";
+			else if (p.ConnectLeft == false)
+				sPiece += "false, ";
+			else
+				sPiece += "null, ";
+			if (p.ConnectRight == true)
+				sPiece += "true)";
+			else if (p.ConnectRight == false)
+				sPiece += "false) ";
+			else
+				sPiece += "null)";
+			return sPiece;
+		}
+
 		enum Moves { Up, Down, Left, Right, None };
-		static Piece[] Pieces = new Piece[16]
+		static Piece[] Pieces = new Piece[15]
 			{
-			new Piece(false, false, false, false),  //none
-			new Piece(true, false, false, false),  //b
-			new Piece(false, true, false, false),  //t
-			new Piece(false, false, true, false),  //l
-			new Piece(false, false, false, true),  //r
-			new Piece(true, false, true, false),  //bl
-			new Piece(false, true, true, false),  //tl
-			new Piece(false, false, true, true),  //lr
-			new Piece(true, true, false, false),  //bt
-			new Piece(false, true, false, true),  //tr
-			new Piece(true, false, false, true),  //br
-			new Piece(true, true, false, true),  //btr
-			new Piece(true, true, true, false),  //btl
-			new Piece(false, true, true, true),  //tlr
-			new Piece(true, false, true, true),  //blr
-			new Piece(true, true, true, true)   //btlr
-			};
+            //new Piece(false, false, false, false),  //none
+            new Piece(true, false, false, false),  //b
+            new Piece(false, true, false, false),  //t
+            new Piece(false, false, true, false),  //l
+            new Piece(false, false, false, true),  //r
+            new Piece(true, false, true, false),  //bl
+            new Piece(false, true, true, false),  //tl
+            new Piece(false, false, true, true),  //lr
+            new Piece(true, true, false, false),  //bt
+            new Piece(false, true, false, true),  //tr
+            new Piece(true, false, false, true),  //br
+            new Piece(true, true, false, true),  //btr
+            new Piece(true, true, true, false),  //btl
+            new Piece(false, true, true, true),  //tlr
+            new Piece(true, false, true, true),  //blr
+            new Piece(true, true, true, true)   //btlr
+            };
 		public static Piece[][] DungeonSetup(int W, int H)
 		{
 			Piece[][] Width = new Piece[W + 2][];
@@ -74,13 +104,14 @@ namespace DungeonGenerator
 		public static Piece[][] DungeonFill(Piece[][] m)
 		{
 			Random random = new Random();
+			Piece NullPiece = new Piece();
 			int w = random.Next(1, m.Length - 1);
 			int h = random.Next(1, m[0].Length - 1);
 			bool placing = true;
 			bool NoMoves = false;
-			
 
-			while(placing)
+
+			while (placing)
 			{
 				var LastMove = Moves.None;
 
@@ -91,7 +122,7 @@ namespace DungeonGenerator
 				Piece cPiece = m[w][h];
 
 				//placing a piece
-				if (cPiece.Equals(new Piece()))
+				if (cPiece.Equals(NullPiece) && NoMoves == false)
 				{
 					List<Piece> select = Pieces.ToList();
 					#region LINQ
@@ -99,7 +130,7 @@ namespace DungeonGenerator
 					{
 						select = select
 						.Where(p => p.ConnectBottom == true)
-						.Select(p => p).ToList();
+						.ToList();
 					}
 
 					if (Above.ConnectBottom == true)
@@ -137,7 +168,7 @@ namespace DungeonGenerator
 						.Select(p => p).ToList();
 					}
 
-					if (Left.ConnectRight == true)
+					if (Left.ConnectRight == false)
 					{
 						select = select
 						.Where(p => p.ConnectLeft == false)
@@ -150,43 +181,84 @@ namespace DungeonGenerator
 						.Where(p => p.ConnectRight == false)
 						.Select(p => p).ToList();
 					}
+
 					#endregion LINQ
 					if (select.Count != 0)
 					{
 						Piece tPiece = select[random.Next(0, select.Count - 1)];
-						cPiece = tPiece;
+						cPiece.ConnectBottom = tPiece.ConnectBottom;
+						cPiece.ConnectTop = tPiece.ConnectTop;
+						cPiece.ConnectLeft = tPiece.ConnectLeft;
+						cPiece.ConnectRight = tPiece.ConnectRight;
+						//PrintDung(m, w, h);
 					}
-						
 					else
 						NoMoves = true;
 				}
-				//moving to a new block
-				else if(cPiece.GetType() == new Piece().GetType())
+				//moving to a new null block to place
+				else if (!NoMoves)
 				{
-					if (Left.Equals(new Piece()) && cPiece.ConnectLeft == true)
+					if (Left.Equals(NullPiece) && cPiece.ConnectLeft == true)
 						w -= 1;
-					else if (Right.Equals(new Piece()) && cPiece.ConnectRight == true)
+
+					else if (Right.Equals(NullPiece) && cPiece.ConnectRight == true)
 						w += 1;
-					else if (Above.Equals(new Piece()) && cPiece.ConnectTop == true)
+
+					else if (Above.Equals(NullPiece) && cPiece.ConnectTop == true)
 						h -= 1;
-					else if (Below.Equals(new Piece()) && cPiece.ConnectBottom == true)
+
+					else if (Below.Equals(NullPiece) && cPiece.ConnectBottom == true)
 						h += 1;
+
 					else
 						NoMoves = true;
+
+					//PrintDung(m, w, h);
 				}
-				else if(NoMoves)
+
+				//move to a block than can place something
+				else if (NoMoves)
 				{
-					while(NoMoves)
+					int wStart = w;
+					int hStart = h;
+					int visited = 0;
+					int maxVisit = m[w].Length * m.Length * 10;
+					
+
+					//PrintDung(m, w, h);
+
+					while (NoMoves)
 					{
-						if (cPiece.Equals(new Piece()) && Above.ConnectBottom == false && Left.ConnectRight == false && Right.ConnectLeft == false && Below.ConnectTop == false)
+						Above = m[w][h - 1];
+						Below = m[w][h + 1];
+						Left = m[w - 1][h];
+						Right = m[w + 1][h];
+						cPiece = m[w][h];
+
+						if (LastMove != Moves.None && wStart == w && hStart == h)
+							visited++;
+
+						if (visited == maxVisit)
 						{
+							NoMoves = false;
 							placing = false;
-							NoMoves = false;
 						}
-						else if((cPiece.ConnectTop == true && Above.ConnectBottom == true)||(cPiece.ConnectLeft == true && Left.ConnectRight == true) ||(cPiece.ConnectRight == true && Right.ConnectLeft == true) ||(cPiece.ConnectBottom == true && Below.ConnectTop == true))
+
+
+						if (cPiece.Equals(NullPiece))
 						{
 							NoMoves = false;
 						}
+						//if a move can be made after !noMoves block
+						else if (
+							(cPiece.ConnectTop == true && Above.ConnectBottom == null)
+							|| (cPiece.ConnectLeft == true && Left.ConnectRight == null)
+							|| (cPiece.ConnectRight == true && Right.ConnectLeft == null)
+							|| (cPiece.ConnectBottom == true && Below.ConnectTop == null))
+						{
+							NoMoves = false;
+						}
+						//moves until a move can be made
 						else
 						{
 							int PossibleMoves = 0;
@@ -311,7 +383,7 @@ namespace DungeonGenerator
 								}
 							}
 
-							else if(PossibleMoves == 3)
+							else if (PossibleMoves == 3)
 							{
 								int r = random.Next(0, 2);
 								if (LastMove == Moves.Down)
@@ -415,7 +487,7 @@ namespace DungeonGenerator
 												h -= 1;
 												LastMove = Moves.Up;
 											}
-											
+
 											break;
 										case 2:
 											if (cPiece.ConnectTop == true)
@@ -459,7 +531,7 @@ namespace DungeonGenerator
 												w -= 1;
 												LastMove = Moves.Left;
 											}
-												break;
+											break;
 										case 1:
 											if (cPiece.ConnectLeft == true)
 											{
@@ -562,7 +634,7 @@ namespace DungeonGenerator
 
 							else if (PossibleMoves == 2)
 							{
-								
+
 								if (LastMove == Moves.Down)
 								{
 									if (cPiece.ConnectBottom == true)
@@ -580,7 +652,7 @@ namespace DungeonGenerator
 										w += 1;
 										LastMove = Moves.Right;
 									}
-									
+
 								}
 
 								else if (LastMove == Moves.Up)
@@ -640,7 +712,7 @@ namespace DungeonGenerator
 									}
 								}
 							}
-							
+
 							else if (PossibleMoves == 1)
 							{
 								if (cPiece.ConnectBottom == true)
@@ -662,22 +734,136 @@ namespace DungeonGenerator
 								{
 									h -= 1;
 									LastMove = Moves.Up;
-								}	
+								}
+								else
+								{
+									NoMoves = false;
+									placing = false;
+								}
 							}
 							else
 							{
 								placing = false;
 							}
 						}
-						
-					}
-				}
-				else
-				{
 
+					}
 				}
 			}
 			return m;
+		}
+
+		public static void PrintDung(Piece[][] Map, int w, int h)
+		{
+			Piece Edge = new Piece(false, false, false, false);
+
+			for (int i = 0; i < Map.Length; i++)
+			{
+				for (int j = 0; j < Map[i].Length; j++)
+				{
+					Console.ResetColor();
+					if(j == w && i ==h)
+					{
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.BackgroundColor = ConsoleColor.Red;
+					}
+
+					if (Map[j][i] != null && Edge.Equals(Map[j][i]))
+						if (j == Map[j].Length - 1)
+							Console.WriteLine("e");
+						else
+							Console.Write("e");
+					else if (!Map[j][i].Equals(new Piece()))
+					{
+						Piece p = Map[j][i];
+						if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("┼");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("┴");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("├");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("┤");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("┬");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("└");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("┘");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("─");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("┌");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("┐");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("│");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("^");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("v");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("]");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("[");
+					}
+					else
+						Console.Write(".");
+				}
+			}
+		}
+
+		public static void PrintDung(Piece[][] Map)
+		{
+			Piece Edge = new Piece(false, false, false, false);
+
+			for (int i = 0; i < Map.Length; i++)
+			{
+				for (int j = 0; j < Map[i].Length; j++)
+				{
+					if (Map[j][i] != null && Edge.Equals(Map[j][i]))
+						if (j == Map[j].Length - 1)
+							Console.WriteLine("e");
+						else
+							Console.Write("e");
+					else if (!Map[j][i].Equals(new Piece()))
+					{
+						Piece p = Map[j][i];
+						if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("┼");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("┴");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("├");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("┤");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("┬");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == true)
+							Console.Write("└");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("┘");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("─");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("┌");
+						else if (p.ConnectBottom == true && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("┐");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("│");
+						else if (p.ConnectBottom == true && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("^");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == false && p.ConnectTop == true)
+							Console.Write("v");
+						else if (p.ConnectBottom == false && p.ConnectLeft == true && p.ConnectRight == false && p.ConnectTop == false)
+							Console.Write("]");
+						else if (p.ConnectBottom == false && p.ConnectLeft == false && p.ConnectRight == true && p.ConnectTop == false)
+							Console.Write("[");
+					}
+					else
+						Console.Write(".");
+				}
+			}
 		}
 	}
 }
